60d9c9308cf24818cfdc942d9dbf3cbfe6bfecef
ayushyadav8900@gmail.com <ayushyadav8900@gmail.com>
2026-02-07 21:16:06 +0530

    update order queue and worker

--- src/queues/order.queue.js
+++ src/queues/order.queue.js
@@ -1,10 +1,11 @@
 const Queue = require('bull')
 
-const orderQueue = new Queue("order-queue",{
-    redis: {
-        host: "127.0.0.1",
-        port: 6379
-    }
-})
+const orderQueue = new Queue("order-queue", {
+  redis: {
+    host: process.env.REDIS_HOST || "redis",
+    port: 6379,
+  },
+});
+
 
 module.exports = orderQueue
